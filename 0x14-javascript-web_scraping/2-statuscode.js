7c0b7abd6de6a1d94e1b66ba911753e643665f46
Imaolabiyi <sheron4krist@yahoo.com>
2024-05-21 04:22:15 -0700

    Status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
-
 const request = require('request');
-const args = process.argv;
+const url = process.argv[2];
 
-request(args[2], function (error, response, body) {
-  if (error) { console.log(error); }
-  console.log('code:', response && response.statusCode);
+request.get(url, (error, response) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(`code: ${response.statusCode}`);
+  }
 });
